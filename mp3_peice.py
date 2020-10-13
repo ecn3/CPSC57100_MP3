6d92d48fa0c8b0656f5f7f1210c10c8d16fd1d8c
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-10-13 16:48:15 -0500

    base

---
+++ mp3_peice.py
@@ -0,0 +1,16 @@
+""" 
+Christian Nelson
+10/10/2020
+CPSC-57100-002, Fall 2020
+Machine Problem 3: Course Planning using a Constraint Satisfaction Problem Formulation
+"""
+
+# Imports
+import pandas as pd
+import numpy as np
+from constraint import *
+
+
+# Print heading
+print("CLASS: Artificial Intelligence, Lewis University")
+print("NAME: Christian Nelson")
